33f96dc680a8133a5830334cabfa180bdc149e41
Marat Khasanov <Hinspared@MacBook-Air-Marat.local>
2023-01-19 19:50:31 +0100

    updated contacts section

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const list = [
   {
     name: "phone",
-    value: "+420 608 725 616",
+    value: "+420608725616",
   },
   {
     name: "github",
@@ -27,7 +27,38 @@ const Contacts = () => {
         <div className="absolute left-[-0.5rem] bottom-1 -z-10 h-2 w-56 rounded-sm bg-[#0077FF]"></div>
       </div>
       <p className="mt-3 ml-1 font-light">Let&apos;s have a chat</p>
-      <div className="mt-10 flex justify-between"></div>
+      <div className="mt-16 flex flex-col md:grid md:grid-cols-2 md:gap-16">
+        {React.Children.toArray(
+          list.map((item) => (
+            <div className="flex flex-col">
+              <div className="relative">
+                <p className="relative z-20 text-2xl font-light">{item.name}</p>
+                <div className="absolute left-[-0.5rem] bottom-[3px] z-10 h-6 w-6 rounded-sm bg-[#0077FF]"></div>
+              </div>
+              {item.name === "phone" ? (
+                <a href={`tel:${item.value}`} className="mt-3">
+                  +420 608 725 616
+                </a>
+              ) : null}
+              {item.name === "github" ? (
+                <a href={item.value} className="mt-3">
+                  {item.value}
+                </a>
+              ) : null}
+              {item.name === "email" ? (
+                <a href={`mailto:${item.value}`} className="mt-3">
+                  {item.value}
+                </a>
+              ) : null}
+              {item.name === "linkedin" ? (
+                <a href={item.value} className="mt-3">
+                  Marat Khasanov
+                </a>
+              ) : null}
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 };
