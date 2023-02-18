bea308d3976e2be12352b82f9143a77e33e17e06
Marat Khasanov <Hinspared@MacBook-Air-Marat.local>
2023-02-18 21:05:08 +0100

    add delay to useOnScreen hook

--- src/utils/hooks/useOnScreen.ts
+++ src/utils/hooks/useOnScreen.ts
@@ -1,13 +1,29 @@
 import React from "react";
 
-const useOnScreen = (ref: React.RefObject<HTMLElement>, threshold = 0.8) => {
+const useOnScreen = (
+  ref: React.RefObject<HTMLElement>,
+  threshold = 0.8,
+  delay = 100
+) => {
   const [isVisible, setIsVisible] = React.useState(false);
 
   React.useEffect(() => {
+    let timeout: NodeJS.Timeout | undefined;
+
     const observer = new IntersectionObserver(
       ([entry]) => {
         const newEntry = entry as IntersectionObserverEntry;
-        setIsVisible(newEntry.isIntersecting);
+        // detect if element is visible with delay
+        if (newEntry.isIntersecting) {
+          timeout = setTimeout(() => {
+            setIsVisible(true);
+          }, delay);
+        } else {
+          if (timeout) {
+            clearTimeout(timeout);
+          }
+          setIsVisible(false);
+        }
       },
       {
         threshold,
@@ -15,14 +31,14 @@ const useOnScreen = (ref: React.RefObject<HTMLElement>, threshold = 0.8) => {
     );
 
     const currentElement = ref.current as Element;
-
     if (currentElement) {
       observer.observe(currentElement);
     }
+
     return () => {
       observer.unobserve(currentElement);
     };
-  }, [ref, threshold]);
+  }, [delay, ref, threshold]);
 
   return isVisible;
 };
